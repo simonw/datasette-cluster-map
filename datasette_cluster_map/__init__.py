1fc2884b4e81d19c8d5e578b81b0ddd78a74e322
Simon Willison <simonw@eventbrite.com>
2018-04-20 06:24:09 -0700

    Upgrade to latest Leaflet / Leaflet.markercluster

--- datasette_cluster_map/__init__.py
+++ datasette_cluster_map/__init__.py
@@ -4,24 +4,23 @@ from datasette import hookimpl
 @hookimpl
 def extra_css_urls():
     return [{
-        "url": "https://unpkg.com/leaflet@1.0.3/dist/leaflet.css",
-        "sri": "sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ=="
+        "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
+        "sri": "sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
     }, {
-        "url": "https://unpkg.com/leaflet.markercluster@1.0.3/dist/MarkerCluster.css",
+        "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css",
         "sri": "sha384-lPzjPsFQL6te2x+VxmV6q1DpRxpRk0tmnl2cpwAO5y04ESyc752tnEWPKDfl1olr"
     }, {
-        "url": "https://unpkg.com/leaflet.markercluster@1.0.3/dist/MarkerCluster.Default.css",
+        "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css",
         "sri": "sha384-5kMSQJ6S4Qj5i09mtMNrWpSi8iXw230pKU76xTmrpezGnNJQzj0NzXjQLLg+jE7k"
     }]
 
 
 @hookimpl
 def extra_js_urls():
-	return [{
-        "url": "https://unpkg.com/leaflet@1.0.3/dist/leaflet-src.js",
-        "sri": "sha512-WXoSHqw/t26DszhdMhOXOkI7qCiv5QWXhH9R7CgvgZMHz1ImlkVQ3uNsiQKu5wwbbxtPzFXd1hK4tzno2VqhpA=="
+    return [{
+        "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
+        "sri": "sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
     }, {
-        "url": "https://unpkg.com/leaflet.markercluster@1.0.3/dist/leaflet.markercluster-src.js",
-        "sri": "sha384-aPW7+bcmswg0D7N22za0Cj4RDQtJjUVz9obDfEeT0q8ekjOqn32IJt8Hmxqjl6jV"
-    },
-    "/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js"]
+        "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster-src.js",
+        "sri": "sha384-NAOEbWFcjnXc7U9GkULPhupHZNAbqru9dS3c+4ANYAwtFoVAWuVuMVDH0DIy4ESp"
+    }, "/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js"]
