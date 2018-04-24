simonw/datasette-cluster-map

commit 25db0f2aaa539f86f6f737f986770efd812e643d

Apply plugin to table.rows-and-columns
See https://github.com/simonw/datasette/commit/00ab95430003768067bb496b125a2be93134e73d

datasette_cluster_map/static/datasette-cluster-map.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('body.table,body.row,body.query')) {
         // Does it have Latitude and Longitude columns?
         let columns = Array.prototype.map.call(
-            document.querySelectorAll('th'),
+            document.querySelectorAll('table.rows-and-columns th'),
             (th) => {
                 return th.className.replace(/^col\-/, '')
             }
@@ -121,7 +121,7 @@ const addClusterMap = (latitudeColumn, longitudeColumn) => {
         zoom: 13,
         layers: [tiles]
     });
-    let table = document.getElementsByTagName('table')[0];
+    let table = document.getElementsByTagName('table.rows-and-columns')[0];
     table.parentNode.insertBefore(el, table);
     let progressDiv = document.createElement('div');
     progressDiv.style.marginBottom = '2em';

commit dec8658b5b9b2a8172aae000088f84bc62685216

Fixed dumb bug introduced in 25db0f2aaa539f86f6f737f986770efd812e643d

datasette_cluster_map/static/datasette-cluster-map.js
@@ -121,7 +121,7 @@ const addClusterMap = (latitudeColumn, longitudeColumn) => {
         zoom: 13,
         layers: [tiles]
     });
-    let table = document.getElementsByTagName('table.rows-and-columns')[0];
+    let table = document.querySelector('table.rows-and-columns');
     table.parentNode.insertBefore(el, table);
     let progressDiv = document.createElement('div');
     progressDiv.style.marginBottom = '2em';