f31c2bab0bdee9839cd5771a6d3620756c74dd78
Simon Willison <swillison@gmail.com>
2019-01-17 22:37:47 -0800

    Code formatting using black

--- datasette_cluster_map/__init__.py
+++ datasette_cluster_map/__init__.py
@@ -3,27 +3,35 @@ from datasette import hookimpl
 
 @hookimpl
 def extra_css_urls():
-    return [{
-        "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
-        "sri": "sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
-    }, {
-        "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css",
-        "sri": "sha384-lPzjPsFQL6te2x+VxmV6q1DpRxpRk0tmnl2cpwAO5y04ESyc752tnEWPKDfl1olr"
-    }, {
-        "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css",
-        "sri": "sha384-5kMSQJ6S4Qj5i09mtMNrWpSi8iXw230pKU76xTmrpezGnNJQzj0NzXjQLLg+jE7k"
-    }]
+    return [
+        {
+            "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
+            "sri": "sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==",
+        },
+        {
+            "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css",
+            "sri": "sha384-lPzjPsFQL6te2x+VxmV6q1DpRxpRk0tmnl2cpwAO5y04ESyc752tnEWPKDfl1olr",
+        },
+        {
+            "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css",
+            "sri": "sha384-5kMSQJ6S4Qj5i09mtMNrWpSi8iXw230pKU76xTmrpezGnNJQzj0NzXjQLLg+jE7k",
+        },
+    ]
 
 
 @hookimpl
 def extra_js_urls():
-    return [{
-        "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
-        "sri": "sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
-    }, {
-        "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster-src.js",
-        "sri": "sha384-NAOEbWFcjnXc7U9GkULPhupHZNAbqru9dS3c+4ANYAwtFoVAWuVuMVDH0DIy4ESp"
-    }, "/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js"]
+    return [
+        {
+            "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
+            "sri": "sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==",
+        },
+        {
+            "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster-src.js",
+            "sri": "sha384-NAOEbWFcjnXc7U9GkULPhupHZNAbqru9dS3c+4ANYAwtFoVAWuVuMVDH0DIy4ESp",
+        },
+        "/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js",
+    ]
 
 
 @hookimpl
--- setup.py
+++ setup.py
@@ -1,35 +1,28 @@
 from setuptools import setup
 import os
 
-VERSION = '0.4'
+VERSION = "0.4"
 
 
 def get_long_description():
-    with open(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'README.md'
-    ), encoding='utf8') as fp:
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
         return fp.read()
 
 
 setup(
-    name='datasette-cluster-map',
-    description='Datasette plugin that shows a cluster map for any data with latitude/longitude columns',
+    name="datasette-cluster-map",
+    description="Datasette plugin that shows a cluster map for any data with latitude/longitude columns",
     long_description=get_long_description(),
-    long_description_content_type='text/markdown',
-    author='Simon Willison',
-    url='https://github.com/simonw/datasette-cluster-map',
-    license='Apache License, Version 2.0',
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-cluster-map",
+    license="Apache License, Version 2.0",
     version=VERSION,
-    packages=['datasette_cluster_map'],
-    entry_points={
-        'datasette': [
-            'cluster_map = datasette_cluster_map'
-        ],
-    },
-    package_data={
-        'datasette_cluster_map': [
-            'static/datasette-cluster-map.js',
-        ],
-    },
-    install_requires=['datasette']
+    packages=["datasette_cluster_map"],
+    entry_points={"datasette": ["cluster_map = datasette_cluster_map"]},
+    package_data={"datasette_cluster_map": ["static/datasette-cluster-map.js"]},
+    install_requires=["datasette"],
 )
