a1ec67b5b3bf5cf315b14cefe2238ced82c909b9
Simon Willison <swillison@gmail.com>
2019-11-01 20:27:48 -0700

    Upgrade Leaflet and leaflet.markercluster

--- datasette_cluster_map/__init__.py
+++ datasette_cluster_map/__init__.py
@@ -5,15 +5,15 @@ from datasette import hookimpl
 def extra_css_urls():
     return [
         {
-            "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
-            "sri": "sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==",
+            "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.css",
+            "sri": "sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==",
         },
         {
-            "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css",
+            "url": "https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css",
             "sri": "sha384-lPzjPsFQL6te2x+VxmV6q1DpRxpRk0tmnl2cpwAO5y04ESyc752tnEWPKDfl1olr",
         },
         {
-            "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css",
+            "url": "https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css",
             "sri": "sha384-5kMSQJ6S4Qj5i09mtMNrWpSi8iXw230pKU76xTmrpezGnNJQzj0NzXjQLLg+jE7k",
         },
     ]
@@ -23,12 +23,12 @@ def extra_css_urls():
 def extra_js_urls():
     return [
         {
-            "url": "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
-            "sri": "sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==",
+            "url": "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js",
+            "sri": "sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==",
         },
         {
-            "url": "https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster-src.js",
-            "sri": "sha384-NAOEbWFcjnXc7U9GkULPhupHZNAbqru9dS3c+4ANYAwtFoVAWuVuMVDH0DIy4ESp",
+            "url": "https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster-src.js",
+            "sri": "sha384-N9K+COcUk7tr9O2uHZVp6jl7ueGhWsT+LUKUhd/VpA0svQrQMGArhY8r/u/Pkwih",
         },
         "/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js",
     ]
