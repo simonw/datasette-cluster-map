893e16ed196a898ba284c2f8c67f6e9a768e0132
Simon Willison <swillison@gmail.com>
2021-01-23 14:48:35 -0800

    All tests now use datasette.client

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=["datasette_cluster_map"],
     entry_points={"datasette": ["cluster_map = datasette_cluster_map"]},
     package_data={"datasette_cluster_map": ["static/datasette-cluster-map.js"]},
-    install_requires=["datasette>=0.48"],
+    install_requires=["datasette>=0.50"],
     extras_require={"test": ["pytest", "pytest-asyncio", "httpx", "sqlite-utils"]},
     tests_require=["datasette-cluster-map[test]"],
 )
--- tests/test_cluster_map.py
+++ tests/test_cluster_map.py
@@ -2,7 +2,6 @@ from datasette_cluster_map import extra_css_urls, extra_js_urls
 from datasette.app import Datasette
 import pytest
 import sqlite_utils
-import httpx
 
 
 @pytest.fixture(scope="session")
@@ -92,14 +91,11 @@ def db_path(tmp_path_factory):
     ],
 )
 async def test_plugin_config(db_path, config, table, expected_fragments):
-    app = Datasette(
-        [db_path], metadata={"plugins": {"datasette-cluster-map": config}}
-    ).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/test/{}".format(table))
-        assert response.status_code == 200
-        for fragment in expected_fragments:
-            assert fragment in response.text
+    ds = Datasette([db_path], metadata={"plugins": {"datasette-cluster-map": config}})
+    response = await ds.client.get("/test/{}".format(table))
+    assert response.status_code == 200
+    for fragment in expected_fragments:
+        assert fragment in response.text
 
 
 @pytest.mark.asyncio
@@ -119,37 +115,38 @@ async def test_plugin_config(db_path, config, table, expected_fragments):
 async def test_plugin_only_on_tables_or_queries_with_columns(
     db_path, path, should_have_javascript
 ):
-    app = Datasette([db_path]).app()
+    ds = Datasette([db_path])
     fragments = ("/datasette-cluster-map.js", "window.DATASETTE_CLUSTER_MAP_TILE_LAYER")
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost{}".format(path))
-        assert response.status_code == 200
-        if should_have_javascript:
-            for fragment in fragments:
-                assert fragment in response.text
-        else:
-            for fragment in fragments:
-                assert fragment not in response.text
+    response = await ds.client.get(path)
+    assert response.status_code == 200
+    if should_have_javascript:
+        for fragment in fragments:
+            assert fragment in response.text
+    else:
+        for fragment in fragments:
+            assert fragment not in response.text
 
 
 @pytest.mark.asyncio
 async def test_plugin_is_installed():
-    app = Datasette([], memory=True).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/plugins.json")
-        assert response.status_code == 200
-        installed_plugins = {p["name"] for p in response.json()}
-        assert "datasette-cluster-map" in installed_plugins
-        # Check JavaScript is correctly served
-        response = await client.get(
-            "http://localhost/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js"
-        )
-        assert response.status_code == 200
-        assert "const clusterMapEscapeHTML" in response.text
+    ds = Datasette([], memory=True)
+    response = await ds.client.get("/-/plugins.json")
+    assert response.status_code == 200
+    installed_plugins = {p["name"] for p in response.json()}
+    assert "datasette-cluster-map" in installed_plugins
+    # Check JavaScript is correctly served
+    response = await ds.client.get(
+        "/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js"
+    )
+    assert response.status_code == 200
+    assert "const clusterMapEscapeHTML" in response.text
 
 
 @pytest.mark.asyncio
 async def test_respects_base_url():
     ds = Datasette([], memory=True, config={"base_url": "/foo/"})
     response = await ds.client.get("/:memory:?sql=select+1+as+latitude,+2+as+longitude")
-    assert '<script src="/foo/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js"></script>' in response.text
+    assert (
+        '<script src="/foo/-/static-plugins/datasette_cluster_map/datasette-cluster-map.js"></script>'
+        in response.text
+    )
